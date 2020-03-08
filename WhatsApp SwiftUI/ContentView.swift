e6339fcb26e487a0764fa2ef1bf644afe970ea24
mohamed gamal <mohamedgamalmohamed48@gmail.com>
2020-03-08 14:34:30 +0200

    Initial Commit

---
+++ WhatsApp SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhatsApp SwiftUI
+//
+//  Created by mohamed gamal on 3/8/20.
+//  Copyright © 2020 mohamed gamal. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
